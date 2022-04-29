65c8122efa1a229c0e2cee59cfaa3239ae6bfe01
Amkam <amaduskamara@gmail.com>
2022-04-29 18:22:43 +0000

    Add redux store and configuration

---
+++ src/redux/greetings/greetings.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+// conts
+const FETCH_GREETING = "hello-rails-react/greetings/FETCH_GREETING";
+
+// actions
+const fetchGreeting = (payload) => ({
+  type: FETCH_GREETING,
+  payload,
+});
+
+// state
+const initialState = [];
+
+export const fetchGreetingApi = () => async (dispatch) => {
+  const returnValue = await axios.get("http://localhost:3000/v1/greetings");
+  const greeting = returnValue.data.data.greeting.message;
+  console.log(greeting);
+  dispatch(fetchGreeting(greeting));
+};
+
+// reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_GREETING:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
